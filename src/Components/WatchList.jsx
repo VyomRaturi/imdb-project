7afec379c372613f4c655b4b49532d847b2365b9
VyomRaturi <vyomraturi2004@gmail.com>
2025-05-23 00:01:38 +0530

    searching, sorting and filtering

--- src/Components/WatchList.jsx
+++ src/Components/WatchList.jsx
@@ -1,8 +1,33 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import genreids from "../assets/genre";
 
 const WatchList = () => {
   const [watchlist, setWatchlist] = useState([]);
+  const [search, setSearch] = useState("");
+  const [genreList, setGenreList] = useState([]);
+  const [currGenre, setCurrGenre] = useState("All");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleFilter = (genre) => {
+    setCurrGenre(genre);
+  }
+
+  const handleAscendingRatings = () => {
+    let sortedAscending = watchlist.sort((a, b) => {
+      return a.vote_average - b.vote_average;
+    });
+    setWatchlist([...sortedAscending]);
+  };
+
+  const handleDescendingRatings = () => {
+    let sortedDescending = watchlist.sort((a, b) => {
+      return b.vote_average - a.vote_average;
+    });
+    setWatchlist([...sortedDescending]);
+  };
 
   useEffect(() => {
     let moviesFromLocalStorage = localStorage.getItem("watchList");
@@ -11,19 +36,72 @@ const WatchList = () => {
       return;
     }
 
-    setWatchlist(JSON.parse(moviesFromLocalStorage));
-    console.log(JSON.parse(moviesFromLocalStorage)); 
+    const parsedWatchList = JSON.parse(moviesFromLocalStorage);
+
+    setWatchlist(parsedWatchList);
+    console.log(parsedWatchList);
   }, []);
+
+  useEffect(() => {
+    console.log(watchlist);
+    let genres = watchlist.map((movie) => {
+      return genreids[movie.genre_ids[0]];
+    });
+
+    console.log(genres);
+    genres = new Set(genres);
+    setGenreList(["All",...genres]);
+    console.log(genres);
+  }, [watchlist]);
 
   return (
     <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+      {/* Search feild */}
+      <div className="flex justify-center my-10">
+        <input
+          type="text"
+          placeholder="Search movies"
+          value={search}
+          onChange={handleSearch}
+          className="border border-gray-500 h-[3rem] w-[18rem] px-3"
+        />
+      </div>
+
+      {/* Genre */}
+      <div className="flex justify-center m-4">
+        {genreList.map((genre) => {
+          return (
+            <div
+              key={genre}
+              onClick={() => handleFilter(genre)}
+              className={
+                currGenre == genre
+                  ? "mx-4 flex justify-center items-center bg-blue-400 h-[3rem] w-[9rem] text-white font-bold border rounded-xl cursor-pointer"
+                  : "mx-4 flex justify-center items-center bg-gray-400 h-[3rem] w-[9rem] text-white border rounded-xl cursor-pointer"
+              }
+            >
+              {genre}
+            </div>
+          );
+        })}
+      </div>
+
+      {/* Watchlist table */}
       <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
         <thead>
           <tr className="bg-gray-50">
             <th className="px-6 py-4 font-medium text-gray-900">Name</th>
             <th>
-              <div className="flex">
+              <div className="flex items-center gap-2">
+                <i
+                  onClick={handleAscendingRatings}
+                  className="fa-solid fa-arrow-up"
+                ></i>
                 <div>Ratings</div>
+                <i
+                  onClick={handleDescendingRatings}
+                  className="fa-solid fa-arrow-down"
+                ></i>
               </div>
             </th>
             <th>
@@ -39,23 +117,34 @@ const WatchList = () => {
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-          {watchlist.map((movieObj) => (
-            <tr key={movieObj.id} className="hover:bg-gray-50">
-              <td className="flex items-center px-6 py-4 font-normal text-gray-900">
-                <img
-                  className="h-[6rem] w-[10rem] object-fit"
-                  src={`https://image.tmdb.org/t/p/original/${movieObj.backdrop_path}`}
-                  alt="movie poster"
-                />
-                <div className="font-medium ml-3 text-gray-700 text-sm">
-                  {movieObj.title}
-                </div>
-              </td>
-              <td className="pl-6 py-4">{movieObj.vote_average}</td>
-              <td className="pl-6 py-4">{movieObj.popularity}</td>
-              <td className="pl-2 py-4">{genreids[movieObj.genre_ids[0]]}</td>
-            </tr>
-          ))}
+          {watchlist
+            .filter((movie) => {
+              if (currGenre === "All") {
+                return true;
+              }
+
+              return genreids[movie.genre_ids[0]] === currGenre;
+            })
+            .filter((movie) =>
+              movie.title.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((movieObj) => (
+              <tr key={movieObj.id} className="hover:bg-gray-50">
+                <td className="flex items-center px-6 py-4 font-normal text-gray-900">
+                  <img
+                    className="h-[6rem] w-[10rem] object-fit"
+                    src={`https://image.tmdb.org/t/p/original/${movieObj.backdrop_path}`}
+                    alt="movie poster"
+                  />
+                  <div className="font-medium ml-3 text-gray-700 text-sm">
+                    {movieObj.title}
+                  </div>
+                </td>
+                <td className="pl-6 py-4">{movieObj.vote_average}</td>
+                <td className="pl-6 py-4">{movieObj.popularity}</td>
+                <td className="pl-2 py-4">{genreids[movieObj.genre_ids[0]]}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
